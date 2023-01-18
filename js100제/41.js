5fc666b1dc26094a7d145b6d1bd227e5adc01607
TaeWon Park <thisis_tw94@naver.com>
2023-01-18 20:25:44 +0900

    add : js100제

--- js100제/41.js
+++ js100제/41.js
@@ -2,12 +2,21 @@
 // 소수이면 YES로, 소수가 아니면 NO로 출력해주세요.
 // (소수 : 1과 자기 자신만으로 나누어떨어지는 1보다 큰 양의 정수)
 
-function distinction (i) {
-  if(i % 2 === 0) {
-    console.log('Yes')
-  } else {
-      console.log('No')
+function distinction (num) {
+  for (let i=2; i<num; i++) {
+      const res = num % i;
+      if(res === 0) {
+        console.log('NO')
+        return false;
+      }
   }
-}
+  if(num === 1) {
+    conosloe.log('No');
+    return;
+  }
+  console.log('Yes')
+  }
+
 
 distinction(3)
+
