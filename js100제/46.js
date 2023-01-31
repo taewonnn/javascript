88a997e4de11ca774c585d65750dbf14a5195c49
TaeWon Park <thisis_tw94@naver.com>
2023-01-31 21:02:07 +0900

    add : js100제

--- js100제/46.js
+++ js100제/46.js
@@ -11,5 +11,12 @@ function sumNum() {
     arrSum[i] = i+1;
   }
 
-  arrSum
+  arrSum.forEach((n) => {
+    while(n !== 0){
+      sum += (n % 10);
+      n = Math.floor(n/10);
+    }
+  })
+
+  console.log(arrSum);
 }
