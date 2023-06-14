c91f9807a974e373d1ed2b0f2b08b72b14e40b55
TaeWon Park <thisis_tw94@naver.com>
2023-06-14 23:42:36 +0900

    coding test

--- js100제/49.js
+++ js100제/49.js
@@ -6,3 +6,24 @@
 // 입력 : 10 9 8 7 6 5 4 3 2 1
 // 출력 : 10
 // ```
+
+
+// let numbers = prompt('10개의 숫자를 입력하세요').split(' ').map((n) => {
+//   return parseInt(n, 10);
+// });
+//
+// numbers.sort((a, b) => {
+//   return b-a;
+// });
+//
+// console.log(numbers[0]);
+
+
+
+let n = [1,2,3,4,5,6,7,8,9,10]
+function maxmax(n) {
+  num = n.split('')
+  console.log(num)
+}
+
+maxmax(n)
